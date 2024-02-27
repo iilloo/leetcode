7111e598f7b2c66f33ce0bb351d705c2085c2f6d
iilloo <wanghp5520jlu@outlook.com>
2024-02-27 17:10:12 +0800

    637二叉树的层次遍历的衍生题目

---
+++ 637.二叉树的层平均值.cpp
@@ -0,0 +1,48 @@
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <deque>
+#include <list>
+#include <string>
+using namespace std;
+/*
+ * @lc app=leetcode.cn id=637 lang=cpp
+ *
+ * [637] 二叉树的层平均值
+ */
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    vector<double> averageOfLevels(TreeNode *root)
+    {
+        vector<int> res;
+        queue<TreeNode*> tq;
+        if (root) {
+            tq.push(root);
+        }
+    }
+};
+// @lc code=end
