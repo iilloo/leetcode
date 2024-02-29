bd9b68c6de33defd097e947951b348a9c323ceeb
iilloo <wanghp5520jlu@outlook.com>
2024-02-29 16:02:42 +0800

    104.递归法求解

--- 104.二叉树的最大深度.cpp
+++ 104.二叉树的最大深度.cpp
@@ -65,5 +65,43 @@ public:
         }
         return depth;
     }
+
+    //递归法 后序遍历法,求高度的过程
+    int maxDepth_1(TreeNode *root) {
+        if (!root) {
+            return 0;
+        }
+        int ld = maxDepth_1(root->left);
+        int rd = maxDepth_1(root->right);
+        return ld >= rd ? ld + 1 : rd + 1; 
+    }
+
+    //递归法，前序遍历法，求深度的过程
+    int result;
+    void getDepth(TreeNode* node, int depth) {
+        result = depth > result ? depth : result;
+        if (!(node->left) && !(node->right)) {
+            return;
+        }
+        if (node->left) {
+            ++depth;
+            getDepth(node->left, depth);
+            --depth;
+        }
+        if (node->right) {
+            ++depth;
+            getDepth(node->right, depth);
+            --depth;
+        }
+        return;
+    }
+    int maxDepth_2(TreeNode *root) {
+        result = 0;
+        if (!root) {
+            return 0;
+        }
+        getDepth(root, 1);
+        return result;
+    }
 };
 // @lc code=end
