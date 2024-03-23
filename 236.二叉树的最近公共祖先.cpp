61ff9c1bd4beafaa5e11395ecc8308bd6a0dce35
iilloo <wanghp5520jlu@outlook.com>
2024-03-23 12:41:00 +0800

    236.wancheng

--- 236.二叉树的最近公共祖先.cpp
+++ 236.二叉树的最近公共祖先.cpp
@@ -37,7 +37,24 @@ struct TreeNode
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        
+        if (root == NULL) {
+            return NULL;
+        }
+        if (root == p || root == q) {
+            return root;
+        }
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        if (left != NULL && right != NULL) {
+            return root;
+        }
+        if (left != NULL && right == NULL) {
+            return left;
+        }
+        if (left == NULL && right != NULL) {
+            return right;
+        }
+        return NULL;
     }
 };
 // @lc code=end
