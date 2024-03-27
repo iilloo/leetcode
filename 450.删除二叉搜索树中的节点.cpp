155157aa3b60ef173550575deaa02d83a22f1766
iilloo <wanghp5520jlu@outlook.com>
2024-03-27 19:16:33 +0800

    wancheng 450.

--- 450.删除二叉搜索树中的节点.cpp
+++ 450.删除二叉搜索树中的节点.cpp
@@ -37,11 +37,83 @@ struct TreeNode
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-    TreeNode* deleteNode(TreeNode* root, int key) {
-
+    TreeNode *deleteNode(TreeNode *root, int key)
+    {
+        if (root == NULL)
+        {
+            return NULL;
+        }
+        if (root->val > key)
+        {
+            root->left = deleteNode(root->left, key);
+        }
+        else if (root->val < key)
+        {
+            root->right = deleteNode(root->right, key);
+        }
+        else
+        {
+            if (root->left == NULL && root->right == NULL)
+            {
+                return NULL;
+            }
+            if (root->left == NULL && root->right != NULL)
+            {
+                return root->right;
+            }
+            if (root->left != NULL && root->right == NULL)
+            {
+                return root->left;
+            }
+            if (root->left != NULL && root->right != NULL)
+            {
+                TreeNode *r = root->right;
+                while (r->left != NULL)
+                {
+                    r = r->left;
+                }
+                r->left = root->left;
+                return root->right;
+            }
+        }
+        return root;
+    }
+    // 迭代法
+    TreeNode* deleteOneNode(TreeNode* target) {
+        if (target == nullptr) return target;
+        if (target->right == nullptr) return target->left;
+        TreeNode* cur = target->right;
+        while (cur->left) {
+            cur = cur->left;
+        }
+        cur->left = target->left;
+        return target->right;
+    }
+    TreeNode *deleteNode_1(TreeNode *root, int key)
+    {
+        if (root == nullptr) return root;
+        TreeNode* cur = root;
+        TreeNode* pre = nullptr; // 记录cur的父节点，用来删除cur
+        while (cur) {
+            if (cur->val == key) break;
+            pre = cur;
+            if (cur->val > key) cur = cur->left;
+            else cur = cur->right;
+        }
+        if (pre == nullptr) { // 如果搜索树只有头结点
+            return deleteOneNode(cur);
+        }
+        // pre 要知道是删左孩子还是右孩子
+        if (pre->left && pre->left->val == key) {
+            pre->left = deleteOneNode(cur);
+        }
+        if (pre->right && pre->right->val == key) {
+            pre->right = deleteOneNode(cur);
+        }
+        return root;
     }
 };
 // @lc code=end
-
