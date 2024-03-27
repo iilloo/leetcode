0eee3a98c18a4e6ca4655602bacc9982e803c000
iilloo <wanghp5520jlu@outlook.com>
2024-03-27 13:30:18 +0800

    kaishi 450.

---
+++ 450.删除二叉搜索树中的节点.cpp
@@ -0,0 +1,47 @@
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <list>
+#include <string>
+#include <algorithm>
+#include <iterator>
+#include <iostream>
+using namespace std;
+/*
+ * @lc app=leetcode.cn id=450 lang=cpp
+ *
+ * [450] 删除二叉搜索树中的节点
+ */
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+
+    }
+};
+// @lc code=end
+
